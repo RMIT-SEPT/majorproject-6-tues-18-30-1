05f0209a82d782efd14ef9d46d708213cc8437e9
Liam <s3507109@student.rmit.edu.au>
2020-08-14 15:10:04 +1000

    Spring Boot backend created

---
+++ BackEnd/Spring Boot/src/main/java/com/example/Tues18301Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tues18301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tues18301Application.class, args);
+	}
+
+}
---
+++ BackEnd/Spring Boot/src/test/java/com/example/Tues18301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tues18301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
