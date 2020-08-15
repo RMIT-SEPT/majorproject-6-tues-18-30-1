bb701459ea9aab65e8e33e731d02fabc4a912915
Liam <s3507109@student.rmit.edu.au>
2020-08-15 20:25:45 +1000

    Revert "Rebuilt SpringBoot backend"
    This reverts commit 6cd429a232f39a404b004b5295bc10743118c980.

--- BackEnd/Spring Boot/src/main/java/product/Repositories/ProductRepository.java
+++ BackEnd/Spring Boot/src/main/java/product/Repositories/ProductRepository.java
@@ -1,15 +1,12 @@
-package com.backend.product.Repositories;
+package product.Repositories;
 
 import org.springframework.stereotype.Repository;
-
 import org.springframework.data.repository.CrudRepository;
-import com.backend.product.model.Product;
+import product.model.Product;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
     @Override
     Iterable<Product> findAllById(Iterable<Long> iterable);
-
-	Product save(Product product);
 }
