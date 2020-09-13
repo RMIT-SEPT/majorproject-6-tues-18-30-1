58711ff3da1abd1b80a6cff8a9b6de4967ce32a3
Liam <s3507109@student.rmit.edu.au>
2020-09-14 04:56:51 +1000

    Front-End UI initial build
    not linked with API stuff yet

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { useState } from "react";
+import ReactDOM, { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+
+import { Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import "./App.css";
+
+import Header from "./components/Header";
+import CustomerLogInForm from "./components/CustomerLogInForm";
+import CustomerSignUpForm from "./components/CustomerSignUpForm";
+import BusinessLogInForm from "./components/BusinessLogInForm";
+import BusinessSignupForm from "./components/BusinessSignupForm";
+
+// const homePage = (
+//   <div id="options">
+//     <h2>I am a...</h2>
+//     <Button>Customer</Button>
+//     <Button>Business</Button>
+//   </div>
+// );
+
+// class HomePage extends Component {
+//   render() {
+//     return homePage;
+//   }
+// }
+
+class App extends Component {
+  state = {
+    customerLogin: false,
+    businessLogin: false,
+    customerSignUp: false,
+    businessSignUp: false,
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <div className="App">
+            <Header />
+            <div class="buttons" align="center">
+              <Button
+                style={{ fontSize: 30 }}
+                color="danger"
+                onClick={() => {
+                  this.setState({ customerLogin: true, businessLogin: false });
+                }}
+              >
+                Customer
+              </Button>
+              <span></span>
+              <Button
+                style={{ fontSize: 30 }}
+                color="warning"
+                onClick={() => {
+                  this.setState({ businessLogin: true, customerLogin: false });
+                }}
+              >
+                Business owner
+              </Button>
+            </div>
+            <div class="customer-log-in" align="center">
+              <Switch>
+                <Route path="/" exact="true">
+                  {this.state.customerLogin ? <CustomerLogInForm /> : null}
+                </Route>
+              </Switch>
+            </div>
+            <div class="customer-sign-up" align="center">
+              <Switch>
+                <Route path="/" exact="true">
+                  {this.state.customerSignUp ? <CustomerSignUpForm /> : null}
+                </Route>
+              </Switch>
+            </div>
+            <div class="business-log-in" align="center">
+              <Switch>
+                <Route path="/" exact="true">
+                  {this.state.businessLogin ? <BusinessLogInForm /> : null}
+                </Route>
+              </Switch>
+            </div>
+
+            <div class="business-sign-up" align="center">
+              <Switch>
+                <Route path="/" exact="true">
+                  {this.state.businessSignUp ? <BusinessSignupForm /> : null}
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
+
+// class AppClass extends Component {
+//   state = {
+//     isLoading: true,
+//     groups: []
+//   };
+
+//   async successfulMount() {
+//     const reply = await fetch ('users');
+//     const body = await response.json();
+//     this.setState({ groups: body, isLoading: false });
+//   }
+// }
+
+// render() {
+//   const {groups, isLoading} = this.state;
+
+//   if(isLoading) {
+//     return <p>Processing your request...</p>;
+//   }
+
+//   return (
+//     <div className=""
+//   )
+// }
 
 export default App;
