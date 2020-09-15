82f012a726b5107f3b6770c7a2482e55d432e33a
Matthew Elliott <matthew98901@gmail.com>
2020-09-15 15:15:07 +1000

    Adding service CRUD functionality

--- BackEnd/Spring Boot/src/main/java/com/example/model/Service.java
+++ BackEnd/Spring Boot/src/main/java/com/example/model/Service.java
@@ -22,4 +22,36 @@ public class Service {
     public Service() {
 
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
--- BackEnd/Spring Boot/src/main/java/com/example/web/ServiceController.java
+++ BackEnd/Spring Boot/src/main/java/com/example/web/ServiceController.java
@@ -1,4 +1,57 @@
 package com.example.web;
 
+import com.example.model.Service;
+import com.example.services.ServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/services")
 public class ServiceController {
+
+    @Autowired
+    private ServiceService serviceService;
+
+    @PostMapping("")
+    public ResponseEntity<Service> createNewService(@RequestBody Service service) {
+        Service service1 = serviceService.saveOrUpdateService(service);
+        return new ResponseEntity<Service>(service, HttpStatus.CREATED);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ArrayList<Service>> returnServices() {
+        ArrayList<Service> services = new ArrayList<Service>();
+        services = serviceService.getServices();
+        return new ResponseEntity<ArrayList<Service>>(services, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Service> getService(@PathVariable Long id) {
+        Optional<Service> service = serviceService.getService(id);
+        if (service.isPresent()) {
+            return new ResponseEntity<Service>(service.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Service> replaceService(@RequestBody Service service, @PathVariable Long id) {
+        service.setId(id);
+        Service service1 = serviceService.saveOrUpdateService(service);
+        return new ResponseEntity<Service>(service, HttpStatus.CREATED);
+    }
+
+    // Really need to think about how deletes should work, this is probably violating the database pretty hard
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteService(@PathVariable Long id) {
+        if (serviceService.deleteService(id)) {
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
 }
