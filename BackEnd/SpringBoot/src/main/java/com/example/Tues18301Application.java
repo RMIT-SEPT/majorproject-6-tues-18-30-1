60e505ec10e29862dafa32585df2bfe54909c3b1
Matthew Elliott <matthew98901@gmail.com>
2020-09-22 22:10:16 +1000

    Basic unit testing added

--- BackEnd/SpringBoot/src/main/java/com/example/Tues18301Application.java
+++ BackEnd/SpringBoot/src/main/java/com/example/Tues18301Application.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ public class Tues18301Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tues18301Application.class, args);
+
 	}
 
 }
